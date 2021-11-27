065d3c18f38671b89ede899b06f7696f456a45d0
River Diniz <river-s-diniz@hotmail.com>
2021-11-27 00:34:43 -0400

    Finalizado
    Projeto final, finalizado.

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,22 @@ const App = () => {
     { id: 2, name: 'Osvaldo o chatão', done: true },
   ]);
 
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
 
-        <AddArea />
+        <AddArea onEnter={handleAddTask} />
 
         {list.map((item, index)=>(
           <ListItem key={index} item={item} />
