68bc6cdb3e712d1da16086659c3ba89a7e903826
River Diniz <river-s-diniz@hotmail.com>
2021-11-27 00:01:21 -0400

    Novas pasta
    Foi criado uma pasta dentro de components,
    
    - AddArea,
    index.tsx
    style.ts.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item';
 import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
@@ -14,7 +15,7 @@ const App = () => {
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
 
-        {/* Área de adicionar nova tarefa */}
+        <AddArea />
 
         {list.map((item, index)=>(
           <ListItem key={index} item={item} />
@@ -26,4 +27,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default App;;
---
+++ src/components/AddArea/index.tsx
@@ -0,0 +1,13 @@
+import * as C from './styles';
+
+export const AddArea = () => {
+    return (
+        <C.Container>
+            <div className="image">+</div>
+            <input
+                type="text"
+                placeholder="Adicione uma tarefa"
+            />
+        </C.Container>
+    );
+}
